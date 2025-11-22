0d82c92b6a80c6b986d9b61a8b38398d052a2681
khanhmf <khanhmf@brothergroup.net>
2025-11-22 17:24:58 +0700

    update base sign in, sign up

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:pizza_app/screens/Home/home_screen.dart';
+import 'package:pizza_app/screens/auth/views/welcome_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -6,8 +10,25 @@ class MyAppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Pizza App",
-      
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return const HomeScreen();
+          } else {
+            return WelcomeScreen();
+          }
+        },
+      ),
     );
   }
 }
